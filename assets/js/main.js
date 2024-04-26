6e3f1b024ece89c2a6cdc11272741b08d40fafa2
Developer-Nasim <ajnasim72@gmail.com>
2024-04-26 22:39:24 +0600

    working home page

---
+++ assets/js/main.js
@@ -0,0 +1,90 @@
+(function($) {
+  "use strict";
+  
+ // menu 
+  $('.siteBar-btn').click( function (){ 
+    $('.mobile-menu').toggleClass('siteBar');
+    $('.menuShadow').toggleClass('show');
+  }); 
+
+    
+  // All Speaches
+  $(".allspeaches").owlCarousel({
+    loop: true,
+    margin: 30,
+    items: 4,
+    rtl: true,
+    navText: [
+      '<img src="assets/img/arrow-left.svg" alt="">',
+      '<img src="assets/img/arrow-right.svg" alt="">'
+    ],
+    nav: true,
+    dots: false,
+    responsive: {
+      0: {
+        items: 1
+      },
+      767: {
+        items: 3
+      },
+      992: {
+        items: 4
+      }
+    }
+  });
+
+    
+  // All Articales
+  $(".all-articales").owlCarousel({
+    loop: true,
+    margin: 30,
+    items: 3,
+    rtl: true,
+    navText: [
+      '<img src="assets/img/arrow-left.svg" alt="">',
+      '<img src="assets/img/arrow-right.svg" alt="">'
+    ],
+    nav: true,
+    dots: false,
+    responsive: {
+      0: {
+        items: 1
+      },
+      767: {
+        items: 2
+      },
+      992: {
+        items: 3
+      }
+    }
+  });
+
+  // Gallery
+  $(".gallery-items").owlCarousel({
+    loop: true,
+    margin: 30,
+    items: 3,
+    rtl: true,
+    stagePadding: 100,
+    navText: [
+      '<img src="assets/img/arrow-left.svg" alt="">',
+      '<img src="assets/img/arrow-right.svg" alt="">'
+    ],
+    nav: true,
+    dots: false,
+    responsive: {
+      0: {
+        items: 1
+      },
+      767: {
+        items: 2
+      },
+      992: {
+        items: 3
+      }
+    }
+  });
+ 
+  
+ 
+})(jQuery);
