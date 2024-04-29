d2e19a6a4b10efeb23401370f9b64908dc4ea3dd
Developer-Nasim <ajnasim72@gmail.com>
2024-04-29 15:42:27 +0600

    last up

--- assets/js/main.js
+++ assets/js/main.js
@@ -86,16 +86,30 @@
     }
   });
   
+  // nice select
+  if (document.querySelectorAll("select").length > 0) {
+    $('select').niceSelect();
+  }
 
   // Popup
-  $('.varea a,.popupVideoShow').magnificPopup({
-    disableOn: 700,
-    type: 'iframe',
-    mainClass: 'mfp-fade',
-    removalDelay: 160,
-    preloader: false,
-
-    fixedContentPos: false
-});
+  $(".varea a,.popupVideoShow").on('click', function(e) {
+    e.preventDefault();
+    const target = e.target
+    $("#video-popup-overlay,#video-popup-iframe-container,#video-popup-container,#video-popup-close").show();
+    
+    var url= target.href+'?autoplay=1';
+    console.log(url)
+     
+    $("#video-popup-iframe").attr('src', url);
+    
+    $("#video-popup-iframe").on('load', function() {
+      $("#video-popup-container").show();
+    });
+  });
+  
+  $("#video-popup-close, #video-popup-overlay").on('click', function(e) {
+    $("#video-popup-iframe-container,#video-popup-container,#video-popup-close,#video-popup-overlay").hide();
+    $("#video-popup-iframe").attr('src', '');
+  });
  
 })(jQuery);
